c0143d4434bce1573669868392a3113e2bcec175
Jorge Jardim <jorge@jorgejardim.com.br>
2018-12-08 18:35:53 -0200

    test-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./pages/home/home.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+
+const routes: Routes = [
+    {
+        path: '', component: ContactComponent,
+        children: [
+            {
+                path: 'login', loadChildren: './pages/contact/contact.module#ContactModule',
+                data: {
+                    breadcrumb: 'Contato'
+                },
+            }
+        ]
+    },
+    {
+        path: '', component: HomeComponent,
+        children: [
+            {
+                path: 'home', loadChildren: './pages/home/home.module#HomeModule',
+                data: {
+                    breadcrumb: 'Início'
+                },
+            }
+        ]
+    },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
